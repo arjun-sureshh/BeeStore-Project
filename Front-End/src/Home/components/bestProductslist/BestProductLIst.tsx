c7e4b73aaf22afb02cc5c3e7c6e7558816183b38
Arjun Suresh <arjunsuresh410@gmail.com>
2025-05-20 11:30:22 +0530

    resolve the  bestproduct.css

--- Front-End/src/Home/components/bestProductslist/BestProductLIst.tsx
+++ Front-End/src/Home/components/bestProductslist/BestProductLIst.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./BestProductLIst.module.css";
+import styles from "./BestProductList.module.css";
 import SingleProduct from "./components/singleProduct/SingleProduct";
 import { Product } from "../types";
 import { useNavigate } from "react-router-dom";
