5debdc1c3b2dbe3655b2b9e1e513cb7421866bd4
Arjun Suresh <arjunsuresh410@gmail.com>
2025-06-08 11:39:38 +0530

    cc

--- Back-End/src/index.js
+++ Back-End/src/index.js
@@ -57,7 +57,7 @@ connectDB()
     app.use("/api/userView", require("./routers/userViewRouters"));
     app.use("/api/productUpload", require("./routers/productUploadRouters"));
 
- app.use("/test-demo", async (req, res) => {
+ app.get("/test-demo", async (req, res) => {
   try {
     const msg="hello backedn connected"
     res.status(200).json(msg);
